c6e1217a27d515947c856f7e6e980ae31fed7c0f
Himakar Perugu <himakarperugu236@gmail.com>
2023-12-04 15:20:43 +0530

    assessments

---
+++ Week5/src_databinding/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'project1';
+
+
+  // isdisabled:boolean=false;
+
+  // ishidden:boolean=false;
+
+
+  // path:string='../assets/3.png';
+
+
+  isactive:boolean=true;
+
+
+
+
+
+
+
+
+
+
+}
